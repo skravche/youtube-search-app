72ee9f03940fae8b5f2c58ada37e23d8e9983779
skravche <sktechno92@gmail.com>
2019-07-30 03:55:26 +0300

    Updates

---
+++ precache-manifest.2350935a236bc599c1b32f7cd7edd149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3695b04111a42f1f63b1ece36a43d170",
+    "url": "/youtube-search-app/index.html"
+  },
+  {
+    "revision": "8ff9a2d5df325401fc99",
+    "url": "/youtube-search-app/static/css/main.ce898bb9.chunk.css"
+  },
+  {
+    "revision": "13fc01c413c767588d95",
+    "url": "/youtube-search-app/static/js/2.e4adac3f.chunk.js"
+  },
+  {
+    "revision": "8ff9a2d5df325401fc99",
+    "url": "/youtube-search-app/static/js/main.57cbe647.chunk.js"
+  },
+  {
+    "revision": "7aa19e5473e97d8eb5ce",
+    "url": "/youtube-search-app/static/js/runtime~main.d18b1bd8.js"
+  }
+]);
